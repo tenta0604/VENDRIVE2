c7f04e36ffb675bb327e5fc910dc55f0bb14d609
tenta0604 <nekotokikanjyu@gmail.com>
2026-09-14 19:38:13 +0900

    chore: remove AN14B3 preview diagnostics

--- api/ocr-web.mjs
+++
@@ -1,9 +0,0 @@
-import { POST as ocrPOST, OPTIONS as ocrOPTIONS } from "./ocr.mjs";
-
-export async function POST(request){
-  return ocrPOST(request);
-}
-
-export async function OPTIONS(request){
-  return ocrOPTIONS(request);
-}
